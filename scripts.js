77ea8800db2e0706583364d8154058460da23dd6
Jón Hafsteinn Guðmundsson <jongudm@gmail.com>
2021-03-30 22:44:27 +0000

    Starting JS work.

---
+++ scripts.js
@@ -0,0 +1,16 @@
+const ENTER_KEYCODE = 13;
+
+const program = (() => {
+
+    function init() {
+
+    };
+
+    return {
+        init
+    };
+})();
+
+document.addEventListener('DOMContentLoaded', () => {
+    program.init();
+});
